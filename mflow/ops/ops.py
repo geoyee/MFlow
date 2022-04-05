7764a4c43324424061d4e4b59e2b3aff2331c4cf
geoyee <geoyee@yeah.net>
2022-04-05 10:38:11 +0800

    [Fix] Update Step's calcJacobi

--- mflow/ops/ops.py
+++ mflow/ops/ops.py
@@ -61,5 +61,4 @@ class Step(Operator):
         self.value = np.mat(np.where(self.nparents[0].value >= 0.0, 1.0, 0.0))
 
     def calcJacobi(self, parent: Any) -> np.matrix:
-        # np.mat(np.eye(self.dim))  # FIXME:
-        return np.zeros(np.where(self.nparents[0].value >= 0.0, 0.0, -1.0))
+        return np.zeros_like(self.nparents[0].value.A1)
