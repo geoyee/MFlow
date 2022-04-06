4614ce74bea38e369342ffd42696fe5bd766215e
geoyee <geoyee@yeah.net>
2022-04-06 11:16:22 +0800

    Update add other opts

--- mflow/opt/opt.py
+++ mflow/opt/opt.py
@@ -1,3 +1,4 @@
+import numpy as np
 from typing import Dict, Union
 from ..core import Graph, Node, getNodeByName, getTrainabledNode
 
@@ -8,6 +9,7 @@ class Optimizer(object):
         self.graph = graph
         self.target = target
         self.lr = lr
+        self.eps = 1e-12
         # Mini Batch中的样本梯度
         self.acc_grads = {}
         self.acc_idx = 0
@@ -80,3 +82,102 @@ class GradientDescent(Optimizer):
         for var_node in var_nodes:
             grad = self.getGrad(var_node)
             var_node.setValue(var_node.value - self.lr * grad)
+
+
+class Momentum(Optimizer):
+    def __init__(self, 
+                 graph: Graph, 
+                 target: Node, 
+                 lr: float=0.01, 
+                 momentum: float=0.9) -> None:
+        super(Momentum, self).__init__(graph, target, lr)
+        self.momentum = momentum
+        self.hist_v = {}  # 累计历史速度
+
+    def _update(self) -> None:
+        var_nodes = getTrainabledNode(self.graph)
+        for var_node in var_nodes:
+            grad = self.getGrad(var_node)
+            # 加入历史记录
+            if var_node not in self.hist_v:
+                self.hist_v[var_node] = -self.lr * grad
+            else:
+                self.hist_v[var_node] = self.momentum * self.hist_v[var_node] - \
+                                        self.lr * grad
+            var_node.setValue(var_node.value + self.hist_v[var_node])
+
+
+class AdaGrad(Optimizer):
+    def __init__(self, graph: Graph, target: Node, lr: float=0.01) -> None:
+        super(AdaGrad, self).__init__(graph, target, lr)
+        self.hist_s = {}
+
+    def _update(self) -> None:
+        var_nodes = getTrainabledNode(self.graph)
+        for var_node in var_nodes:
+            grad = self.getGrad(var_node)
+            # 累计梯度分量的平方和
+            if var_node not in self.hist_s:
+                self.hist_s[var_node] = np.power(grad, 2)
+            else:
+                self.hist_s[var_node] += np.power(grad, 2)
+            var_node.setValue(
+                var_node.value - self.lr * grad / 
+                (np.sqrt(self.hist_s[var_node] + self.eps)))
+
+
+class RMSProp(Optimizer):
+    def __init__(self, 
+                 graph: Graph, 
+                 target: Node, 
+                 lr: float=0.01, 
+                 beta: float=0.9) -> None:
+        super(RMSProp, self).__init__(graph, target, lr)
+        self.beta = beta
+        self.hist_s = {}
+
+    def _update(self) -> None:
+        var_nodes = getTrainabledNode(self.graph)
+        for var_node in var_nodes:
+            grad = self.getGrad(var_node)
+            # 累计梯度分量的平方和
+            if var_node not in self.hist_s:
+                self.hist_s[var_node] = np.power(grad, 2)
+            else:
+                self.hist_s[var_node] = self.beta * self.hist_s[var_node] + \
+                                        (1 - self.beta) * np.power(grad, 2)
+            var_node.setValue(
+                var_node.value - self.lr * grad / 
+                (np.sqrt(self.hist_s[var_node] + self.eps)))
+
+
+class Adam(Optimizer):
+    def __init__(self, 
+                 graph: Graph, 
+                 target: Node, 
+                 lr: float=0.01, 
+                 beta_1: float=0.9, 
+                 beta_2: float=0.99) -> None:
+        super(Adam, self).__init__(graph, target, lr)
+        self.beta_1 = beta_1
+        self.beta_2 = beta_2
+        self.hist_v = {}
+        self.hist_s = {}
+
+    def _update(self) -> None:
+        var_nodes = getTrainabledNode(self.graph)
+        for var_node in var_nodes:
+            grad = self.getGrad(var_node)
+            # 累计梯度分量的平方和
+            if var_node not in self.hist_v:
+                self.hist_v[var_node] = grad
+                self.hist_s[var_node] = np.power(grad, 2)
+            else:
+                self.hist_v[var_node] = self.beta_1 * self.hist_v[var_node] + \
+                                        (1 - self.beta_1) * grad
+                self.hist_s[var_node] = self.beta_2 * self.hist_s[var_node] + \
+                                        (1 - self.beta_2) * np.power(grad, 2)
+
+            var_node.setValue(
+                var_node.value - self.lr * self.hist_v[var_node] / 
+                (np.sqrt(self.hist_s[var_node] + self.eps)))
